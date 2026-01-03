1cb3f08f200d8bf69323aa8590ca9841e610193b
arham-ali1323 <arham.ali1323@gmail.com>
2026-01-03 18:20:49 +0500

    update the contant

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -72,7 +72,7 @@ const Footer = () => {
             </ul>
           </motion.div>
 
-          {/* Contact & Social */}
+          {/* Contact */}
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
